e8dc113cea159ff79b26c651d15ad6e7874a855a
Christine Movius <cmovius@ord.blueraster.com>
2016-05-23 13:54:38 -0400

    Connect stamplay data to app in task store.

--- app/stores/TaskStore.js
+++ app/stores/TaskStore.js
@@ -1,5 +1,6 @@
 import alt from '../lib/alt.js';
 import TaskActions from '../actions/TaskActions.js';
+import * as $ from "axios";
 
 class TaskStore {
   constructor() {
@@ -8,6 +9,15 @@ class TaskStore {
   }
 
   read() {
+    let _this = this;
+    const tasks = this.tasks;
+    $.get("https://todoz.stamplayapp.com/api/cobject/v1/task", { params : { select : "task" } })
+      .then(function(res){
+        let alltasks = res.data.data;
+        _this.setState({
+          tasks : tasks.concat(alltasks)
+        })
+      })
 
   }
   create() {
